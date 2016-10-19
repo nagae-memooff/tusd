05a810bff318540d2e77f8f85dfdec8d549e6721
nagae-memooff <169192@gmail.com>
2016-10-19 17:05:56 +0800

    删除s3相关代码

--- cmd/tusd/cli/composer.go
+++ cmd/tusd/cli/composer.go
@@ -6,13 +6,7 @@ import (
 	"github.com/nagae-memooff/tusd"
 	"github.com/nagae-memooff/tusd/filestore"
 	"github.com/nagae-memooff/tusd/limitedstore"
-	"github.com/nagae-memooff/tusd/memorylocker"
-	"github.com/nagae-memooff/tusd/s3store"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 var Composer *tusd.StoreComposer
@@ -31,17 +25,6 @@ func CreateComposer() {
 
 		store := filestore.New(dir)
 		store.UseIn(Composer)
-	} else {
-		stdout.Printf("Using 's3://%s' as S3 bucket for storage.\n", Flags.S3Bucket)
-
-		// Derive credentials from AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY_ID and
-		// AWS_REGION environment variables.
-		credentials := aws.NewConfig().WithCredentials(credentials.NewEnvCredentials())
-		store := s3store.New(Flags.S3Bucket, s3.New(session.New(), credentials))
-		store.UseIn(Composer)
-
-		locker := memorylocker.New()
-		locker.UseIn(Composer)
 	}
 
 	storeSize := Flags.StoreSize
